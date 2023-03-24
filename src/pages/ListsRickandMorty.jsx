12b2e54413afbfab3182499ee38a478cb5efe28c
Romy Garcia <neitorme@gmail.com>
2023-03-24 14:06:24 -0500

    Commit 3

--- src/pages/ListsRickandMorty.jsx
+++ src/pages/ListsRickandMorty.jsx
@@ -1,17 +1,40 @@
 import axios from 'axios';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom'
 
 const ListsRickandMorty = () => {
-    useEffect(()=> {
+    const navigate = useNavigate();
+    const [dataRick, setDataRick] = useState([])
+    useEffect(() => {
         axios
-        .get("https://rickandmortyapi.com/api/location/")
-        .then((resp)=> console.log(resp))
-        .catch(error => console.error(error))
-    },[])
-    
+            .get("https://rickandmortyapi.com/api/location/")
+            .then((resp) => setDataRick(resp.data))
+            // .then((resp) => setDataRick(resp.data.results))
+            .catch(error => console.error(error))
+    }, [])
+
+    // console.log(dataRick.info?.next)
+    // console.log(dataRick.info?.prev)
+    // console.log(dataRick.results)
     return (
         <div>
-            <h1>LISTADO DE RICK AND MORTY</h1>
+            <h1>Listado Rick and Morty</h1>
+            <Link to={`/ListsRickandMorty/${dataRick.info?.next}`} >
+                <li> Siguiente </li>
+            </Link>
+            <Link to={`/ListsRickandMorty/${dataRick.info?.prev}`} >
+                <li> Anterior </li>
+            </Link>
+            <ul>
+
+                {
+                    dataRick.results?.map(elem => (
+                        <Link key={elem.id} to={`/List_Id/${elem.id}`} >
+                            <li> {elem.name} </li>
+                        </Link>
+                    ))
+                }
+            </ul>
         </div>
     );
 };
