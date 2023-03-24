011711be44587f5c10d8aa9ae8a6a875a090fc95
Romy Garcia <neitorme@gmail.com>
2023-03-23 19:46:20 -0500

    Empezando a crear

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import { useState } from 'react'
 import './App.css'
+import {
+  HashRouter,
+  Routes,
+  Route
+} from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-    <h1>App</h1>
-    </div>
+    <HashRouter>
+      <div className="App">
+        <h1>App</h1>
+      </div>
+      <Routes>
+        <Route path="/" element={<DefaultComponent />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/about/team" element={<Team />} />
+      </Routes>
+    </HashRouter>
+
   )
 }
-
 export default App
